ea5d200c5bc25f138606edb0321da0d5efa8487c
Marc Jakobi <marc@jakobi.dev>
2026-01-14 12:55:53 +0100

    tests: reduce number of packages in bulk install spec

--- spec/operations/bulk_install_spec.lua
+++ spec/operations/bulk_install_spec.lua
@@ -14,11 +14,9 @@ describe("bulk install", function()
     end)
     nio.tests.it("synchronises rocks installations", function()
         local to_install = {
-            { name = "neorg", version = "7.0.0" },
             { name = "sweetie.nvim", version = "2.4.0" },
             { name = "rustaceanvim", version = "4.0.0" },
             { name = "haskell-tools.nvim", version = "3.0.0" },
-            { name = "neotest", version = "5.2.3" },
         }
         local actions = vim.iter(to_install)
             :map(function(entry)
@@ -36,10 +34,8 @@ describe("bulk install", function()
         nio.gather(actions)
         local config_file = fs.read_or_create(config.config_path, "")
         local rocks_toml = tostring(require("toml_edit").parse(config_file))
-        assert.is_not_nil(rocks_toml:find([[neorg = "7%.0%.0"]]))
         assert.is_not_nil(rocks_toml:find([["sweetie%.nvim" = "2%.4%.0"]]))
         assert.is_not_nil(rocks_toml:find([[rustaceanvim = "4%.0%.0"]]))
         assert.is_not_nil(rocks_toml:find([["haskell%-tools%.nvim" = "3%.0%.0"]]))
-        assert.is_not_nil(rocks_toml:find([[neotest = "5%.2%.3"]]))
     end)
 end)
