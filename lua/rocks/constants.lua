46cc1fa0ca7cb3808b2049cb2637aeec1add1fa9
Marc Jakobi <marc@jakobi.dev>
2026-02-04 20:40:21 +0000

    chore(constants.lua): update version variable

--- lua/rocks/constants.lua
+++ lua/rocks/constants.lua
@@ -24,7 +24,7 @@ constants.LUA_VERSION = "5.1"
 
 --- Rocks.nvim version
 ---@type string
-constants.ROCKS_VERSION = "2.47.3"
+constants.ROCKS_VERSION = "2.47.4"
 
 --- Default configuration file contents
 ---@type string
