06c0df5776556c61c181eafd074908839bedc404
Marc Jakobi <marc@jakobi.dev>
2026-04-12 12:58:35 +0200

    refactor(installer): use `lux.lumen-labw.org` url

--- installer.lua
+++ installer.lua
@@ -432,7 +432,7 @@ local function install()
                 "rocks.nvim",
             }
             if supported_arch then
-                table.insert(install_cmd, 4, "--server='https://lumen-oss.github.io/rocks-binaries/'")
+                table.insert(install_cmd, 4, "--server='https://lux.lumen-labs.org/rocks-binaries/'")
             end
             vim.notify("Installing rocks.nvim...")
             local sc = exec(install_cmd)
