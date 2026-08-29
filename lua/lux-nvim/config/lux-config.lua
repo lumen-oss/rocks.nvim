898f2c3f638c850325b504fb3a2f0eafdc5aca50
vhyrro <vhyrro@gmail.com>
2026-08-29 19:12:55 +0200

    fix: use consistent filepaths

--- lua/lux-nvim/config/lux-config.lua
+++ lua/lux-nvim/config/lux-config.lua
@@ -5,7 +5,7 @@ local default_config = lux.config.new()
     :lua_version("5.1")
     :entrypoint_layout({ layout = "nvim" })
     :extra_servers({ "https://lux.lumen-labs.org/rocks-binaries/" })
-    :workspace_tree(vim.fn.stdpath("data") .. "/lux.nvim")
+    :workspace_tree(vim.fn.stdpath("data") .. "/lux")
     :build()
 
 -- TODO(vhyrro): this should read from the lua configuration and adapt the config accordingly
